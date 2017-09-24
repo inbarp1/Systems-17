00335351c47b7ab9ce890cc07dbeba8b1e9af6fb
Inbar Pe'er <inbarpeer@dyn-160-39-222-151.dyn.columbia.edu>
2017-09-24 12:32:33 -0400

    finished

--- array_swap.c
+++ array_swap.c
@@ -1,31 +1,37 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int print_array(int[] the_array){
+int print_array(int ar[10]){
   int i = 0;
   while(i<10){
-    printf("arr[%d]: %d \n",i, the_array[i]);
+    printf("ar[%d]: %d \n",i, ar[i]);
     i++;
   }
+  return 0;
 }
 int main(){
   int a[10];
   int i = 0;
   while( i <9){
     srand(time(NULL));
-    srand(1000);
+    srand(i);
     a[i]=rand();
     i++;
   }
   a[9]=0;
-  printf("The orginal array:\n")
+  printf("The orginal array:\n");
   print_array(a);
   //first array
-  //int b[10];
-//  i=0;
-//  while (i<10){
-  //  b[i]=
-//  }
-
+  int b[10];
+  i=0;
+  int *point;
+  while (i<10){
+    point = &a[9-i];
+    b[i]= *point;
+    i++;
+  }
+  printf("The second array:\n");
+  print_array(b);
+return 0;
 
 }
